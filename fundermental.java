42bc7332a22fa639e1fa102708c57064315d3575
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-08-31 23:53:57 -0400

    Java Variables and Data typs. 🚩

---
+++ fundermental.java
@@ -0,0 +1,19 @@
+public class fundermental {
+    public static void main (String args[]){
+        int age = 12;
+        double salary = 23000.00;
+        // float count = 23.5990;
+        boolean status = true;
+        char shortname = 'A';
+        String name = "Dulash";
+
+        System.out.println(age);
+        System.out.println(salary);
+        // System.out.println(count);
+        System.out.println(status);
+        System.out.println(shortname);
+        System.out.println(name);
+
+
+    }
+}

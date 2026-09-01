b880fc3228e3ced3b8b39be8cad86b1bf25222ee
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-08-31 23:57:36 -0400

    Update. 📌

--- fundermental.java
+++ fundermental.java
@@ -2,14 +2,14 @@ public class fundermental {
     public static void main (String args[]){
         int age = 12;
         double salary = 23000.00;
-        // float count = 23.5990;
+        float count = (float)23.5990;  //castion karanava thama me karala thiyanne
         boolean status = true;
         char shortname = 'A';
         String name = "Dulash";
 
         System.out.println(age);
         System.out.println(salary);
-        // System.out.println(count);
+        System.out.println(count);
         System.out.println(status);
         System.out.println(shortname);
         System.out.println(name);

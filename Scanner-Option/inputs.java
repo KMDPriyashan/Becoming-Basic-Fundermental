9bf4bcab0d99b90d97e64c907a7e4353f981a14d
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-09-01 00:27:45 -0400

    Coeding Update using Scanner class. 🚩

--- Scanner-Option/inputs.java
+++ Scanner-Option/inputs.java
@@ -2,10 +2,22 @@ import java.util.Scanner;
 public class inputs {
     public static void main (String args[]){
         Scanner scan = new Scanner (System.in);
-        System.out.println("Enter Your name :");
+        System.out.print("Enter Your name :");
         String name = scan.nextLine();
 
+        System.out.print("Enter Your Salary :");
+        double salary = scan.nextDouble();
+
+        System.out.print("Enter Your Marrid or Unmarrid? :");
+        boolean marrid = scan.nextBoolean();
+
+        System.out.print("Enter Your started letter :");
+        char nameA = scan.next().charAt(0);
+
         System.out.println("Your name : "+name);
+        System.out.println("Your salary : "+salary);
+        System.out.println("Your marrid : "+marrid);
+        System.out.println("Your stated letter : "+nameA);
         scan.close();
     }
     

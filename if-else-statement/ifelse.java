cdc2ff512e09b1838473d1ee739cd5cead5d34fd
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-09-01 01:22:17 -0400

    if-else statement. 📌

---
+++ if-else-statement/ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+    public static void main(String args[]){
+        int mark = 55;
+        if(mark > 100){
+            System.out.println("A");
+        }else if (mark > 75){
+            System.out.println("B");
+        }else{
+            System.out.println("F");
+        }
+    }
+    
+}

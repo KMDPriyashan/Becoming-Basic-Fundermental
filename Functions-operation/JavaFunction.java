930878f6ee91fc31aafe862149487910e364191e
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-09-01 20:10:38 -0400

    Function creation. 📌

---
+++ Functions-operation/JavaFunction.java
@@ -0,0 +1,17 @@
+public class JavaFunction {
+    public static void main(String args[]) {
+        functionalOparation();
+        calculare();
+    }
+
+    public static void functionalOparation() {
+        System.out.println("This is a function");
+    };
+
+    public static void calculare() {
+        int x = 10;
+        int y = 20;
+        int result = x + y;
+        System.out.println("result is :" + result);
+    };
+};

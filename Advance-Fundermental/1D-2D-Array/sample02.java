ffcc837202ba8ceb2ea1bd0ffd9c9b39082053c7
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-09-04 23:04:42 -0400

    2D array example coding. 🚩

---
+++ Advance-Fundermental/1D-2D-Array/sample02.java
@@ -0,0 +1,15 @@
+// Question 02: 2D Array - Matrix Operations
+//Problem Statement:
+//  Write a Java program that works with a 2D array (matrix) of size 3×3. The program should:
+//  Accept 9 integer values from the user to fill a 3×3 matrix.
+//  Display the matrix in a 3×3 grid format.
+
+//Find and display:
+//  Sum of all elements in the matrix
+//  Sum of the main diagonal (top-left to bottom-right)
+//  Sum of the anti-diagonal (top-right to bottom-left)
+//  Find and display the transpose of the matrix.
+
+public class sample02 {
+    
+}

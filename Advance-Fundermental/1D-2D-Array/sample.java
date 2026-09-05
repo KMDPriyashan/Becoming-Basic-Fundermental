14d86c1dd750816aefe000e9a21c881f915fe09a
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-09-04 22:32:59 -0400

    1d array example code practise. 🚀

---
+++ Advance-Fundermental/1D-2D-Array/sample.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class sample {
+    static int[] arr = new int[10];
+    public static void main (String args[]){
+        Scanner scan = new Scanner (System.in);
+
+        //values ganna eka 
+        for (int x=0; x<10; x++){
+            System.out.println("enter your "+(x+1)+"values :");
+            int value = scan.nextInt();
+            if(value > 100 || value < 0){
+                System.out.println("Invalid number..!");
+                x--;
+            }else{
+                arr[x] = value;
+            }
+        }
+
+        highestValue();
+        lowestValue();
+        everageValue();
+
+    };
+
+    public static void highestValue(){
+        int maxvalue = arr[0];
+        for(int x=0; x<10; x++){
+            if(maxvalue < arr[x]){
+                maxvalue = arr[x];
+            }
+        }
+        System.out.println("Max value is :"+maxvalue);
+    }
+
+    public static void lowestValue(){
+        int minvalue = arr[0];
+        for(int x=0; x<10; x++){
+            if(minvalue > arr[x]){
+                minvalue = arr[x];
+            }
+        }
+        System.out.println("min value is :"+minvalue);
+    }
+
+    public static void everageValue(){
+        int total = 0;
+        for(int x=0; x<10; x++){
+            total = total + arr[x];
+        }
+        double everage = total/arr.length;
+        System.out.println("Evarage value is : "+ everage);
+    }
+}

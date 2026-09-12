6ff822ad1e0a17afd608c1d2811d69b1b4aed057
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-09-11 22:39:57 -0400

    complete the full athenticate function include password checking. ✅

--- Advance-Fundermental/String-Manipulation/Authentication.java
+++ Advance-Fundermental/String-Manipulation/Authentication.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 public class Authentication {
-    
+    static int count1 =0;
+    static int count2 =0;
+    static int count3 =0;
+    static int count4 =0;
+
+
     public static void main (String args[]){
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("enter Your username :");
+        System.out.print("enter Your username :");
         String username = scan.nextLine();
 
-        System.out.println("enter your password : ");
+        System.out.print("enter your password : ");
         String password = scan.nextLine();
 
         usernamecheck(username);
@@ -22,9 +27,9 @@ public class Authentication {
             if(username.contains(" ") || username.contains("@") || username.contains("$") || username.contains("#") || username.contains("&") || username.contains("%") || username.contains("!")){
                 System.out.println("username is invalid.. ! (can't include the space or symbles)");
             }else if(!Character.isLetter(firstletter)){
-                System.out.println("username is invalid.. !");
+                System.out.println("username is invalid.. ! (can start with the digits or symbles)");
             }else{
-                System.out.println("username is valid.. !");
+                System.out.println("Congratulation..! username is valid.. ! Welcome to becomming..... ");
             }
         }else{
             System.out.println("please enter the between 5-15 charcture.. !");
@@ -32,6 +37,27 @@ public class Authentication {
     }
 
     public static void passwordcheck(String password){
+        if(password.length() >= 8){
+            for(int x=0; x<password.length(); x++){
+                char letter = password.charAt(x);
+                if(Character.isUpperCase(letter)){
+                    count1++;
+                }else if(Character.isLowerCase(letter)){
+                    count2++;
+                }else if(Character.isDigit(letter)){
+                    count3++;
+                }else{
+                    count4++;
+                }
+            }
 
+            if(count1 >= 1 && count2 >= 1 && count3 >= 1 && count4 >= 1){
+                System.out.println("Congratulations..! Password is correct.. Authentication passed.....");
+            }else{
+                System.out.println("Password is incorrect..! (must contain at least one uppercase, lowercase, digit, and special character)");
+            }
+        }else{
+            System.out.println("Password is incorrect..! (must be at least 8 characters long)");
+        }
     }
 }
